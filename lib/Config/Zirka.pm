512fb5c0ce9467dcf845462c7e66f535a6c56c00
Boris Däppen <bdaeppen.perl@gmail.com>
2013-11-10 14:30:16 +0100

    excluded parsing logic from code, so that I can comment on it

--- lib/Config/Zirka.pm
+++ lib/Config/Zirka.pm
@@ -6,7 +6,64 @@ use strict;
 use warnings;
 
 use Marpa::R2;
-use Data::Printer;
+
+#########################################
+# HERE ALL THE PARSING LOGIC IS DEFINED #
+#########################################
+
+my $data_structure = ''
+
+# we will have name-value pairs
+.'
+:start              ::= name_value_pairs
+name_value_pairs    ::= name_value_pair+
+'
+# of which each pair has this structure
+.'
+name_value_pair     ::= name assignation value terminator
+'
+# VARIABLE NAME:
+.'
+name                ~   name_start name_end
+name_start          ~   [\w]
+name_end            ~   [\w\d]+
+'
+# ASSIGNATION
+.'
+assignation         ~   assignation_start assignation_end
+assignation_start   ~   [=\-:]
+assignation_end     ~   [>]*
+'
+# VALUE
+.'
+value               ~   val | single_quoted_val | double_quoted_val
+val                 ~   [^\n\s;]+
+single_quoted_val   ~   single_quote val_singleq_content single_quote
+double_quoted_val   ~   double_quote val_doubleq_content double_quote
+'
+# QUOTES
+."
+single_quote        ~   [']
+val_singleq_content ~   [^']+
+"
+.'
+double_quote        ~   ["]
+val_doubleq_content ~   [^"]+
+'
+# TERMINATOR
+.'
+terminator          ~   [;\n\s]*
+'
+# discard whitespace
+.'
+:discard            ~   ws
+ws                  ~   [\s]+
+'
+;
+
+###########
+# METHODS #
+###########
 
 # Constructor of this class
 sub new {
@@ -25,45 +82,14 @@ sub from_string {
 
     my $g = Marpa::R2::Scanless::G->new({
             default_action => '::array',
-            source         => \(<<'END_OF_SOURCE'),
-    
-:start              ::= name_value_pairs
-name_value_pairs    ::= name_value_pair+
-name_value_pair     ::= name assignation value terminator
-
-name                ~   name_start name_end
-name_start          ~   [\w]
-name_end            ~   [\w\d]+
-
-assignation         ~   assignation_start assignation_end
-assignation_start   ~   [=\-:]
-assignation_end     ~   [>]*
-
-value               ~   val | single_quoted_val | double_quoted_val
-val                 ~   [^\n\s;]+
-single_quoted_val   ~   single_quote val_singleq_content single_quote
-double_quoted_val   ~   double_quote val_doubleq_content double_quote
-single_quote        ~   [']
-double_quote        ~   ["]
-val_singleq_content ~   [^']+
-val_doubleq_content ~   [^"]+
-
-terminator          ~   [;\n\s]*
-
-:discard            ~   ws
-ws                  ~   [\s]+
-
-END_OF_SOURCE
+            source         => \$data_structure,
     });
-    
+
     my $re = Marpa::R2::Scanless::R->new({ grammar => $g });
-    
-    print "Trying to parse:\n$in\n\n";
+
     $re->read(\$in);
 
     my $value = ${$re->value};
-    p $value;
-
 
 }
 
